f7534182871a9d350801cf51bfdb90f2987e7948
Istvan Miklos <miklos.istvan2@gmail.com>
2019-09-21 20:03:07 +0200

    :bug: Fixup twitch api changes for now.. issue: 19

--- src/component/FImage.js
+++ src/component/FImage.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Image = ({focused, focusPath, baseURL, game}) => {
   focused = (focused) ? 'imgfocused' : 'imgunfocused'
   return (
-    <Link to={baseURL + game.name}>
+    <Link to={baseURL + game.id}>
       <img src={game.box_art_url.replace("{width}", "420").replace("{height}", "420")} className={focused} />
     </Link>
   )
